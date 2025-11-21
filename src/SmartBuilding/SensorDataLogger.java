2048991c6a6be052cb5594ea09ad603041187d6a
Winindu <winidu.20232406@iit.ac.lk>
2025-11-21 12:21:54 +0530

    commit 5

--- src/SmartBuilding/SensorDataLogger.java
+++ src/SmartBuilding/SensorDataLogger.java
@@ -1,15 +1,30 @@
 package SmartBuilding;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class SensorDataLogger {
     private final String fileName;
-    private final DateTimeFormatter fat = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+//    private final
+    private final DateTimeFormatter fmt = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
 
     public SensorDataLogger(String fileName) {
         this.fileName = fileName;
     }
 
-    public void log(String id, String type, double reading) {
+    public  synchronized void log(String id, String type, double reading) {
+        String timestamp = LocalDateTime.now().format(fmt);
+        String line = String.format("%s\t%s\t%s\t%s", id, type, reading, timestamp);
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(fileName, true))) {
+            bw.write(line);
+            bw.newLine();
+        } catch (IOException e) {
+            System.err.println("[Logger] Error writing to file: " + e.getMessage());
+        }
+
     }
+
 }
